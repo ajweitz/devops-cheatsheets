c6d025393eb66998a83fc7adc99d876ed42abaaf
Alexander Weitz <ajveits@gmail.com>
2021-07-21 08:14:48 +0900

    added cheat sheet for linux commands

---
+++ bash.sh
@@ -0,0 +1,37 @@
+
+# ls
+ls -R #will list all the files in the sub-directories as well
+ls -a #will show the hidden files
+ls -al #will list the files and directories with detailed information like the permissions, size, owner, etc.
+
+# find
+find /home/ -name notes.txt # To find files in /home/
+find . -name notes.txt # To find files in the current directory use, 
+find -type d -name myDir #To look for directories
+
+# grep
+
+# awk
+
+# sed
+
+# du (check spack of a particular file or folder)
+
+# df (check disk space)
+
+# diff (compare 2 files)
+diff file1 file2
+
+# chmod
+
+# chown
+
+# wget 
+
+# uname
+
+# history
+
+# netstat
+
+# ps
