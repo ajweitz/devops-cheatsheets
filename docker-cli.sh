da39a97f4396f85af6cda45613ad9a9ecd4a4e97
Alexander Weitz <ajveits@gmail.com>
2021-07-21 08:44:21 +0900

    added examples of exec and run

--- docker-cli.sh
+++ docker-cli.sh
@@ -1,3 +1,10 @@
 
+
+# docker ps
 $ docker ps # will list IDs of containers
+
+# docker exec
 docker exec -it <mycontainerId> bash # attaching to a container
+
+# docker run
+docker run -t -i <image_name> bash # run interactive container
