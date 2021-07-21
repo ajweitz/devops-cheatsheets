8eb39b3f14a7b7cf02e7fefb78cc0faba921c89c
Alexander Weitz <ajveits@gmail.com>
2021-07-21 10:04:04 +0900

    re-ordered some of the content and added comments

--- python.py
+++ python.py
@@ -1,133 +1,171 @@
+#########
+# IMPORTS
+#########
 import re
 import math
 import copy
 import json
+
+
+#########
+# GENERAL
+#########
 print()  # It is used to display anything on the console.
 # It is used to find the length of a string, list, tuple, dictionary, etc.
 len()
-min()  # It is used to find the minimum value out of multiple values.
-max()  # It is used to find the maximum value out of multiple values.
 type()  # It tells about the data type of a variable.
 # It confirms if a variable is of xyz type. For example, if 5 is int? This function returns True if yes.
 isinstance()
-z = int("3")  # z will be 3 (casting)
+z = int("3")  # cast string into integer
 
 
-# lists
-listExample = ["Tulip", "Jasmine", "Rose", "Lili", "Daisy"]
-listExample2 = list("hello")  # will create array ['h','e','l','l','o']
-
-listExample.append("orange")  # adds to end of list
-listExample.insert(1, "orange")  # insert in index
-listExample.pop(1)
-listExample.pop()
-del listExample[0]  # remove specific index
-listExample.remove("Tulip")  # remove specific value from the array
-print(listExample[-1])  # accessing last item
-print(listExample[2:5])  # range, note that it does not include 5
-print(listExample[2:len(listExample)])  # range from 2 to last item
-listExample.sort()  # sort list
-listExample = listExample + listExample2  # combine lists
-listExample.reverse()  # reverse list
+#######
+# LISTS
+#######
+arr = ["Tulip", "Jasmine", "Rose", "Lili", "Daisy"]
+arr2 = list("hello")  # will create array ['h','e','l','l','o']
+len(arr)  # array length
+arr.append("orange")  # adds to end of list
+arr.insert(1, "orange")  # insert in index
+arr.pop(1)
+arr.pop()
+del arr[0]  # remove specific index
+arr.remove("Tulip")  # remove specific value from the array
+print(arr[-1])  # accessing last item
+print(arr[2:5])  # range, note that it does not include 5
+print(arr[2:len(arr)])  # range from 2 to last item
+arr.sort()  # sort list
+arr = arr + arr2  # combine lists
+arr.reverse()  # reverse list
 # findining at what index we have a value, returns 1
-listExample.index("Jasmine")
+arr.index("Jasmine")
 
+"Tulip" in arr  # if
 # pythonic way to check if a list is empty or not
 # check if list is not  empty
-if listExample:
+if arr:
     pass
 
 # check if list is empty
-if not listExample:
+if not arr:
     pass
 
-# sets
+# List Comprehension
+list = [expression_on_variable_name for variable_name in iterable if some_condition_on_variable_name]
+list = [c.lower() for c in "ABC" if c != 'A']  # will return ['b', 'c']
+squares = [i**2 for i in range(5)]  # will return [0, 1, 4, 9, 16]
+
+
+#######
+# SETS
+#######
 setExample = set()  # empty set
 setExample = {"apple", "banana", "cherry"}
 setExample.add("orange")
 setExample.remove("banana")
+"banana" in setExample  # returns True if banana exists setExample
 
-# tuple
+
+#######
+# TUPLES
+#######
 # can be accessed like arrays, but cannot be modified
 tupleExample = ("one", "two", "three")
 
-# dict
+
+############
+# DICTIONARY
+############
+# aka hashtable aka map
 dictionaryExample = {
     "key1": "value1",
     "key2": "value2",
     "key3": "value3",
     "key4": "value4",
 }
+dictionaryExample["key"] = "value"  # set a key to value
+"key" in dictionaryExample  # returns True if key is in the dictionary
 
-# if x exists dataset
-"banana" in setExample
-"Tulip" in listExample
 
-# Strings
-a = "Hello, World!\nLine2"
-b = f"{a} fstring"
-print(a[1])  # string is Array
-len(a)  # size, length
+############
+# STRINGS
+############
+str = "Hello, World!\nLine2"
+str2 = f"{str} fstring"
+print(str[1])  # string is Array
+len(str)  # size, length
 
 # True/False methods:
-a.isalpha()
-a.isdigit()
-a.isnumeric()
-a.islower()
-a.isupper()
-a.startswith("Hell")  # will return True
+str.isalpha()
+str.isdigit()
+str.isnumeric()
+str.islower()
+str.isupper()
+str.startswith("Hell")  # will return True
 
-a.upper()  # change all letters to uppercase
-a.lower()  # change all letters to lowercase
+str.upper()  # change all letters to uppercase
+str.lower()  # change all letters to lowercase
 
-a.replace("Hello", "Bye")  # will return: 'Bye, World!\nLine2'
+str.replace("Hello", "Bye")  # will return: 'Bye, World!\nLine2'
 
-a.split(",")  # will split into array: ['Hello', ' World!\nLine2']
-a.splitlines()  # will split into array: ['Hello, World!', 'Line2']
-a.strip()  # trim spaces
+str.split(",")  # will split into array: ['Hello', ' World!\nLine2']
+str.splitlines()  # will split into array: ['Hello, World!', 'Line2']
+str.strip()  # trim spaces
 # finds the first occurrence of the specified value. Will return 7 in this case
-a.find("World")
+str.find("World")
 ";".join(a)
 
-# Parse JSON
-# requires import math
-x = '{ "name":"John", "age":30, "city":"New York"}'
-y = json.loads(x)
 
-# Copy
-# requires import copy
-copy.copy(y)  # Return a shallow copy of x onject.
-copy.deepcopy(y)  # Return a deep copy x object
-
+#######
+# FILES
+#######
 # read file
 f = open("demofile.txt", "r")
-print(f.read())
+str = f.read()  # read content of file into str
+f.close()  # close file
 
 # write file
-f = open("demofile3.txt", "w")
+f = open("demofile3.txt", "w")  # Write (overwrite)
 f.write("Woops! I have deleted the content!")
 f.close()
-f = open("demofile2.txt", "a")
+f = open("demofile2.txt", "a")  # Write (append)
 f.write("Now the file has more content!")
 f.close()
 
-# loops
-while True:
-    break  # b reak
-    continue  # continue
 
+######
+# JSON
+######
+# requires import json
+str = '{ "name":"John", "age":30, "city":"New York"}'
+pythonObject = json.loads(str)
+
+
+######
+# COPY
+######
+# requires import copy
+copy.copy(pythonObject)  # Return a shallow copy of x onject.
+copy.deepcopy(pythonObject)  # Return a deep copy x object
+
+
+########
+# Loops
+########
 # while loop
 i = 0
 while i < 6:
     print(i)
     i += 1
+    break  # break
+    continue  # continue
 
 # loop over string
 for x in "banana":
     print(x)
 
 # loop over list with Index
-for index, val in enumerate(listExample):
+for index, val in enumerate(arr):
     print(index, val)
 
 # loop over dict
@@ -140,20 +178,21 @@ for n in range(0, 9):
     print(n)
 
 
-# Math
+######
+# MATH
+######
 # requires import math
 math.floor(x)  # floor
 x ** 3  # x^3
 7 // 2  # floor divide, returns 3
 -x  # changed sign of x
 4 % 3  # modulo, returns 1
+min()  # It is used to find the minimum value out of multiple values.
+max()  # It is used to find the maximum value out of multiple values.
 
-# List Comprehension
-list = [expression_on_variable_name for variable_name in iterable if some_condition_on_variable_name]
-list = [c.lower() for c in "ABC" if c != 'A']  # will return ['b', 'c']
-squares = [i**2 for i in range(5)]  # will return [0, 1, 4, 9, 16]
-
-
+########
+# REGEXP
+########
 # regexp (regular expression)
 # requires import re
 
