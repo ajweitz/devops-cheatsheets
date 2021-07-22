11863a2f1a19557d7813689f109df31816160c11
Alexander Weitz <ajveits@gmail.com>
2021-07-22 11:22:46 +0900

    Added Jenkins examples

---
+++ jenkins/someLib.groovy
@@ -0,0 +1,8 @@
+def someLibFunction(){
+    return "some value"
+}
+def anotherLibFunction(){
+    echo "this is another lib function"
+}
+
+return this //this return is required to be able to load the file it into a Jenkinsfile
