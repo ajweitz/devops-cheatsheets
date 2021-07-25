d0eeb45f78b6be08cebbb02a3d1da8a65b97cccb
Alexander Weitz <ajveits@gmail.com>
2021-07-25 19:39:12 +0900

    added some basic commands example

--- linux/bash.sh
+++ linux/bash.sh
@@ -1,13 +1,38 @@
+# General Commands
+history # show all previous commands
+clear # clear terminal
+pwd # show current directory
+
+
+# cd
+cd .. # go back one level
+cd - # go to previous dir
 
 # ls
-ls -R #will list all the files in the sub-directories as well
-ls -a #will show the hidden files
-ls -al #will list the files and directories with detailed information like the permissions, size, owner, etc.
+ls -R # list all the files in the sub-directories as well
+ls -a # show the hidden files
+ls -al # list the files and directories with detailed information like the permissions, size, owner, etc.
+
+# cp
+cp filename.txt /home/myuser # copy filename.txt to /home/myuser
+cp *.txt /home/myser # copy all matching *.txt files to /home/myuser
+cp -r ./ /home/myser # copy all files from current directory to /home/myuser
+
+# mv
+mv filename.txt newfilename.txt # rename file
+
+# mkdir
+mkdir foo # create directory foo
+mkdir -p foo/bar/some/more/dirs # create dir+subdirs
+
+# rm
+rm filename.txt # remove file
+rm -rf exampledir # remove a directory without promting for confirmation
 
 # find
-find /home/ -name notes.txt # To find files in /home/
-find . -name notes.txt # To find files in the current directory use, 
-find -type d -name myDir #To look for directories
+find /home/ -name notes.txt # find files in /home/
+find . -name notes.txt # find files in the current directory use, 
+find -type d -name myDir # look for directories
 
 # grep
 
@@ -35,3 +60,8 @@ diff file1 file2
 # netstat
 
 # ps
+
+
+## Bash Scripting
+# vars
+x=$(echo "hello world") # assign output of echo command to var x
