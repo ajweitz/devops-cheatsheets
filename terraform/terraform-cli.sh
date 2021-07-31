0bd8a1fb3bc95bc73402d12f709ec996468dc4ae
Alexander Weitz <ajveits@gmail.com>
2021-07-31 19:22:12 +0900

    added a couple of examples of the terraform cli

---
+++ terraform/terraform-cli.sh
@@ -0,0 +1,6 @@
+terraform init # download the resource dependencies
+terraform plan # see what terraform is about to perform
+terraform apply # apply changes
+terraform destroy # destroy the changes previously applied
+
+terraform show # shows terraform state in a human-readable form
