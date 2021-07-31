9387f46dbfe3db48206b7b07970bcaee7f5e2e3f
Alexander Weitz <ajveits@gmail.com>
2021-07-31 19:30:09 +0900

    added state list example

--- terraform/terraform-cli.sh
+++ terraform/terraform-cli.sh
@@ -4,3 +4,4 @@ terraform apply # apply changes
 terraform destroy # destroy the changes previously applied
 
 terraform show # shows terraform state in a human-readable form
+terraform state list # show a list of deployed resources
