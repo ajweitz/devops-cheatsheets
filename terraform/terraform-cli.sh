da4e42bba5ad832d7eaffa0e599a0ead0ecf0d55
Alexander Weitz <ajveits@gmail.com>
2021-07-31 19:39:12 +0900

    added "state show" example

--- terraform/terraform-cli.sh
+++ terraform/terraform-cli.sh
@@ -5,3 +5,4 @@ terraform destroy # destroy the changes previously applied
 
 terraform show # shows terraform state in a human-readable form
 terraform state list # show a list of deployed resources
+terraform state show some.resource.name # show the state of a specific resource
