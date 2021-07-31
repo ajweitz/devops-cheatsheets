4b8efe9c69fdef0f5981efe3ef23f8cd2662f749
Alexander Weitz <ajveits@gmail.com>
2021-07-31 19:40:18 +0900

    added titles to sections

--- terraform/terraform-cli.sh
+++ terraform/terraform-cli.sh
@@ -1,8 +1,10 @@
+# basic
 terraform init # download the resource dependencies
 terraform plan # see what terraform is about to perform
 terraform apply # apply changes
 terraform destroy # destroy the changes previously applied
 
+# deployment information
 terraform show # shows terraform state in a human-readable form
 terraform state list # show a list of deployed resources
 terraform state show some.resource.name # show the state of a specific resource
