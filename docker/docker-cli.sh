692600bf40ce2a57b229f40d9bf0c06fdcde158c
Alexander Weitz <ajveits@gmail.com>
2021-08-14 20:39:03 +0900

    added docker build example

--- docker/docker-cli.sh
+++ docker/docker-cli.sh
@@ -1,7 +1,8 @@
-
+# docker build
+docker build . # build the Dockerfile inside current directory
 
 # docker ps
-$ docker ps # will list IDs of containers
+docker ps # will list IDs of containers
 
 # docker exec
 docker exec -it <mycontainerId> bash # attaching to a container
