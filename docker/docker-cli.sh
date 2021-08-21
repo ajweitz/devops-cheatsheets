aaa27a1be9e4e1734332f2aa6d82cb6a6e00ddda
Alexander Weitz <ajveits@gmail.com>
2021-08-21 16:12:39 +0900

    corrected docker build command, and added docker system example

--- docker/docker-cli.sh
+++ docker/docker-cli.sh
@@ -1,5 +1,5 @@
 # docker build
-docker build . # build the Dockerfile inside current directory
+docker build . -t <IMAGE_NAME>:<IMAGE_LABEL> # build the Dockerfile inside current directory
 
 # docker ps
 docker ps # will list IDs of containers
@@ -9,3 +9,6 @@ docker exec -it <mycontainerId> bash # attaching to a container
 
 # docker run
 docker run -t -i <image_name> bash # run interactive container
+
+# docker system
+docker system df -v # show size of containers and images
