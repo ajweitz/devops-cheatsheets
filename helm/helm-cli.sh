81f1d8423f11b80d1efbd78b1025bf7bdd85b532
Alexander Weitz <ajveits@gmail.com>
2022-07-01 08:09:22 +0900

    added helm create and rollback examples

--- helm/helm-cli.sh
+++ helm/helm-cli.sh
@@ -1,12 +1,18 @@
+# create new chart
+helm create <chart-name>
+
 # lists all installed charts
 helm list 
 
 # Deploy a chart with helm (needs to be in chart folder):
 helm dependency update # downloading the dependencies
-helm upgrade --install <CHART_NAME> ./ # upgrade/install
+helm upgrade --install <chart-name> ./ # upgrade/install
 
 # Uninstall a chart with helm:
-helm uninstall <CHART_NAME>
+helm uninstall <chart-name>
+
+# Rollback:
+helm rollback <chart-name> <version>
 
 # Debug a template
  helm template . # will render the template and show the resulting YAML
